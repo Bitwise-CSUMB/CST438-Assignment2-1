1e764201267d4bca820155d8af782a5a27745179
Christopher McMichael <cmcmichael@csumb.edu>
2024-03-06 21:20:31 -0800

    Followed assignment 2 specifications for editing Assignment, Grade, and Enrollment objects. Uncommented OneToMany relationships in Section.java and uncommented/altered Repository SQL queries for Assignment, Grade and Enrollment.

--- src/main/java/com/cst438/domain/Assignment.java
+++ src/main/java/com/cst438/domain/Assignment.java
@@ -3,6 +3,7 @@ package com.cst438.domain;
 import jakarta.persistence.*;
 
 import java.sql.Date;
+import java.util.List;
 import java.text.SimpleDateFormat;
 
 @Entity
@@ -11,9 +12,52 @@ public class Assignment {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Column(name="assignment_id")
     private int assignmentId;
- 
-    // TODO  complete this class
+    
     // add additional attributes for title, dueDate
+    private String title;
+
+    @Column(name="due_date")
+    private Date dueDate;
+
     // add relationship between assignment and section entities
+    @ManyToOne
+    @JoinColumn(name="section_no", nullable=false)
+    private Section section;
+
+    // One to many relationship to grade
+    @OneToMany(mappedBy="assignment")
+    List<Grade> grades;
+
     // add getter and setter methods
+    public int getAssignmentId() {
+        return this.assignmentId;
+    }
+
+    public void setAssignmentId(int assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getDueDate() {
+        return this.dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Section getSection() {
+        return this.section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
--- src/main/java/com/cst438/domain/Enrollment.java
+++ src/main/java/com/cst438/domain/Enrollment.java
@@ -1,6 +1,7 @@
 package com.cst438.domain;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class Enrollment {
@@ -11,7 +12,52 @@ public class Enrollment {
 	
 	// TODO complete this class
     // add additional attribute for grade
+    private String grade;
+
     // create relationship between enrollment and user entities
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable=false)
+    private User user;
+
     // create relationship between enrollment and section entities
+    @ManyToOne
+    @JoinColumn(name="section_no", nullable=false)
+    private Section section;
+
+    // One to Many relationship between enrollment and grade
+    @OneToMany
+    List<Grade> grades;
+
     // add getter/setter methods
+    public int getEnrollmentId() {
+        return this.enrollmentId;
+    }
+
+    public void setEnrollmentId(int enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+
+    public String getGrade() {
+        return this.grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Section getSection() {
+        return this.section;
+    }
+
+    public void setSection(Section section) {
+        this.section = section;
+    }
 }
