a961808d24e7e82066e3309f3d2aeef87985580e
jeremiahmcgrath <jmcgrath@csumb.edu>
2024-03-25 17:52:50 -0700

    Relocated Unit Tests 1 and 2 & System Test 1

--- src/test/java/com/cst438/controller/SectionControllerUnitTest.java
+++ src/test/java/com/cst438/controller/SectionControllerUnitTest.java
@@ -32,9 +32,6 @@ public class SectionControllerUnitTest {
     @Autowired
     SectionRepository sectionRepository;
 
-    @Autowired
-    AssignmentRepository assignmentRepository;
-
     @Test
     public void addSection() throws Exception {
 
@@ -133,103 +130,6 @@ public class SectionControllerUnitTest {
         // check the expected error message
         String message = response.getErrorMessage();
         assertEquals("course not found cst599", message);
-
-    }
-
-    // Instructor adds a new assignment successfully
-    @Test
-    public void addAssignment() throws Exception {
-        MockHttpServletResponse response;
-
-        // create DTO with data for new assignment.
-        // the primary key, id, is set to 0. it will be
-        // set by the database when the assignment is inserted.
-        AssignmentDTO assignment = new AssignmentDTO(
-            0,
-            "Test Assignment 1",
-            "2024-03-01",
-            "cst363",
-            1,
-            8
-        );
-
-        // issue a http POST request to SpringTestServer
-        // specify MediaType for request and response data
-        // convert assignment to String data and set as request content
-        response = mvc.perform(
-                MockMvcRequestBuilders
-                    .post("/assignments")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(asJsonString(assignment)))
-            .andReturn()
-            .getResponse();
-
-        // check the response code for 200 meaning OK
-        assertEquals(200, response.getStatus());
-
-        // return data converted from String to DTO
-        AssignmentDTO result = fromJsonString(response.getContentAsString(), AssignmentDTO.class);
-
-        // primary key should have a non-zero value from the database
-        assertNotEquals(0, result.id());
-        // check other fields of the DTO for expected values
-        assertEquals("Test Assignment 1", result.title());
-        assertEquals("2024-03-01", result.dueDate().toString());
-        assertEquals("cst363", result.courseId());
-        assertEquals(1, result.secId());
-        assertEquals(8, result.secNo());
-
-        // check the database
-        Assignment a = assignmentRepository.findByAssignmentId(result.id());
-        assertNotNull(a);
-        assertEquals("cst363", a.getSection().getCourse().getCourseId());
-
-        // clean up after test. issue http DELETE request for assignment
-        response = mvc.perform(
-                MockMvcRequestBuilders
-                    .delete("/assignments/"+result.id()))
-            .andReturn()
-            .getResponse();
-
-        assertEquals(200, response.getStatus());
-
-        // check database for delete
-        a = assignmentRepository.findByAssignmentId(result.id());
-        assertNull(a);  // assignment should not be found after delete
-    }
-
-    // Instructor adds a new assignment with a due date past the end date of the class
-    @Test
-    public void addAssignmentBadDueDate() throws Exception {
-        MockHttpServletResponse response;
-
-        // dueDate "2100-03-01" is past the end date of the class
-        AssignmentDTO assignment = new AssignmentDTO(
-            0,
-            "Test Assignment 1",
-            "2100-03-01",
-            "cst363",
-            1,
-            8
-        );
-
-        // issue the POST request
-        response = mvc.perform(
-                MockMvcRequestBuilders
-                    .post("/assignments")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(asJsonString(assignment)))
-            .andReturn()
-            .getResponse();
-
-        // response should be 400, BAD_REQUEST
-        assertEquals(400, response.getStatus());
-
-        // check the expected error message
-        String message = response.getErrorMessage();
-        assertEquals("Bad due date; section 8 timeframe: 2024-01-15 - 2024-05-17", message);
 
     }
 
