a67ca0646cf75715ada7cebf2d0b74f918a3cc8d
Christopher McMichael <cmcmichael@csumb.edu>
2024-04-05 18:00:13 -0700

    Changes to the service proxy

--- src/main/java/com/cst438/service/RegistrarServiceProxy.java
+++ src/main/java/com/cst438/service/RegistrarServiceProxy.java
@@ -23,13 +23,41 @@ public class RegistrarServiceProxy {
 
     @RabbitListener(queues = "gradebook_service")
     public void receiveFromRegistrar(String message)  {
-         //TODO implement this message 
-} 
-
+        try {
+            System.out.println("Receive from Registrar " + message);
+            String[] parts = message.split(" ", 2);
+            switch (parts[0]) {
+                case "addAssignment":
+                    break;
+                case "updateAssignment":
+                    break;
+                case "deleteAssignment":
+                    break;
+                case "addCourse":
+                    break;
+                case "updateCourse":
+                    break;
+                case "deleteCourse":
+                    break;
+                case "addEnrollment":
+                    break;
+                case "updateEnrollment":
+                    break;
+                case "deleteEnrollment":
+                    break;
+                default:
+                    System.out.println("Option not implemented: " + parts[0]);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in receivedFromRegistrar " + e.getMessage());
+        }
+    } 
 
     private void sendMessage(String s) {
+        System.out.println("Gradebook to Registrar " + s);
         rabbitTemplate.convertAndSend(registrarServiceQueue.getName(), s);
     }
+
     private static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
@@ -37,6 +65,7 @@ public class RegistrarServiceProxy {
             throw new RuntimeException(e);
         }
     }
+
     private static <T> T  fromJsonString(String str, Class<T> valueType ) {
         try {
             return new ObjectMapper().readValue(str, valueType);
