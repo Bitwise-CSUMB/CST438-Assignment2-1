b5f90831cd37621e398e41601fa05b2df4677fa4
Christopher McMichael <cmcmichael@csumb.edu>
2024-04-05 18:09:11 -0700

    Added print statements to the proxy

--- src/main/java/com/cst438/service/GradebookServiceProxy.java
+++ src/main/java/com/cst438/service/GradebookServiceProxy.java
@@ -23,12 +23,41 @@ public class GradebookServiceProxy {
 
     @RabbitListener(queues = "registrar_service")
     public void receiveFromGradebook(String message)  {
-         //TODO implement this message 
-} 
+        try {
+            System.out.println("Receive from Gradebook " + message);
+            String[] parts = message.split(" ", 2);
+            switch (parts[0]) {
+                case "addAssignment":
+                    break;
+                case "updateAssignment":
+                    break;
+                case "deleteAssignment":
+                    break;
+                case "addCourse":
+                    break;
+                case "updateCourse":
+                    break;
+                case "deleteCourse":
+                    break;
+                case "addEnrollment":
+                    break;
+                case "updateEnrollment":
+                    break;
+                case "deleteEnrollment":
+                    break;
+                default:
+                    System.out.println("Option not implemented: " + parts[0]);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in receivedFromGradebook " + e.getMessage());
+        }
+    } 
 
     private void sendMessage(String s) {
+        System.out.println("Registrar to Gradebook " + s);
         rabbitTemplate.convertAndSend(gradebookServiceQueue.getName(), s);
     }
+
     private static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
